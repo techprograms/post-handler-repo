699e710d1583f5ccaa2a7b0dea6657770db2ca0b
techprograms <tech.gpr@gmail.com>
2024-10-12 16:53:23 +0530

    post handler api with getpost and deletepost

---
+++ src/components/Posts.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { getPosts, deletePost } from "../services/postService";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts()
+      .then((result) => {
+        setPosts(result.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleDelete = (id) => {
+    deletePost(id)
+      .then(() => {
+        setPosts(posts.filter((post) => post.id !== id));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>All Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <button onClick={() => handleDelete(post.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
